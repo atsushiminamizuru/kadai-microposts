901fbfc0555bd03ae9ece28bd8a2f2bcec5956e7
atsushiminamizuru <utinonekokami19950717@gmail.com>
2023-07-11 16:43:29 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController; // 追記
 use App\Http\Controllers\MicropostsController; //追記
+use App\Http\Controllers\UserFollowController; //追記
 
 /*
 |--------------------------------------------------------------------------
@@ -29,5 +30,12 @@ Route::middleware('auth')->group(function () {
     
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     // 追記この中にまとめた方が効率的
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
+    
+    Route::group(['prefix' => 'users/{id}'], function () {                                          // 追記
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');         // 追記
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow'); // 追記
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings'); // 追記
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');    // 追記
+    }); 
 });
 
